6e0bdf059b1aa37ac1cfd52b62fb7829e51bc5a7
soqutto <me@sokutto.com>
2021-03-09 01:07:12 +0900

    Modified build environment

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,4 +7,27 @@ module.exports = {
     port: 8080,
     open: true,
   },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              //sourceMap: enabledSourceMap
+            }
+          }
+        ]
+      },
+      {
+        test: /\.png/,
+        use:
+          'url-loader'
+      }
+    ]
+  }
 };
