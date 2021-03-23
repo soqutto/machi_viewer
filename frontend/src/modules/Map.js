fb44849b562ff1c65509a374162fd208c2bb0012
soqutto <me@sokutto.com>
2021-03-23 20:44:45 +0900

    Modified map drawing routines using TownParser, async function, SVG structure

--- frontend/src/modules/Map.js
+++ frontend/src/modules/Map.js
@@ -8,6 +8,7 @@ import shadowUrl      from 'leaflet/dist/images/marker-shadow.png'
 import * as d3 from 'd3'
 import * as topojson from 'topojson'
 
+import TownParser from './TownParser'
 const map = L.map("map").setView([35.6492, 139.5493], 13);
 
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
@@ -15,18 +16,32 @@ L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
   maxZoom: 18
 }).addTo(map);
 
-const townSvg = d3.select(map.getPanes().overlayPane).append("svg");
-const townSvgGroup = townSvg.append("g").attr("class", "leaflet-zoom-hide");
+const citySvg = d3.select(map.getPanes().overlayPane).append("svg");
 
-d3.json('./topojson.test/h27ka13208.topojson').then(createTownSvg);
+const main = async () => {
+  await TownParser.load('./topojson.test/h27ka13208.topojson');
+  await TownParser.parse();
+  await TownColorizer.createTable(TownParser.city.getTownAreaList());
+  createTownSvg();
+}
 
-function createTownSvg(tj){
-  const gj = topojson.feature(tj, tj.objects.town);
+function createTownSvg(){
+  const geoJsonCity = topojson.feature(TownParser.json, TownParser.json.objects.city);
+  const geoJsonTown = topojson.feature(TownParser.json, TownParser.json.objects.town);
   const transform = d3.geoTransform({point: projectPoint});
   const path = d3.geoPath().projection(transform);
 
-  const townPolygons = townSvgGroup.selectAll("path")
-                       .data(gj.features)
+  const cityGroup = citySvg.append("g").attr("class", "leaflet-zoom-hide").attr("id", "city");
+
+  const cityFramePolygon = cityGroup.selectAll("path")
+                           .data(geoJsonCity.features)
+                       .enter()
+                       .append("path");
+
+  const townGroup = cityGroup.append("g").attr("class", "leaflet-zoom-hide").attr("id", "town");
+
+  const townPolygons = townGroup.selectAll("path")
+                       .data(geoJsonTown.features)
                        .enter()
                        .append("path");
 
@@ -39,24 +54,32 @@ function createTownSvg(tj){
   }
 
   function update(){
-    const bounds = path.bounds(gj);
+    const bounds = path.bounds(geoJsonCity);
 
     let topLeft = bounds[0], bottomRight = bounds[1];
     console.log(topLeft, bottomRight);
 
-    townSvg.attr("width", bottomRight[0] - topLeft[0])
+    citySvg.attr("width", bottomRight[0] - topLeft[0])
            .attr("height", bottomRight[1] - topLeft[1])
            .style("left", topLeft[0] + "px")
            .style("top", topLeft[1] + "px");
 
-    townSvgGroup.attr("transform", "translate(" + -topLeft[0] + ","
+    cityFramePolygon.attr("d", path)
+                    .style("fill", "none")
+                    .attr("stroke", "#888888")
+                    .attr("stroke-width", "4.0");
+
+    cityGroup.attr("transform", "translate(" + -topLeft[0] + ","
                                                 + -topLeft[1] + ")");
 
-    townPolygons.attr("d", path)
-                .style("fill-opacity", 0.7)
+    townPolygons.attr("class", (d) => "town-" + d.properties.KCODE1)
                 .attr("fill", "skyblue")
-                .attr("stroke", "#aaaaaa")
-                .attr("stroke-width", "1.4");
+                .attr("stroke", "#666666")
+                .attr("stroke-width", "1.4")
+                .style("fill-opacity", 0.5)
+                .attr("d", path);
   }
 
 }
+
+main();
