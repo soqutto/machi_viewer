38aedb520b663232820357b4fbecf593d4a85e5f
soqutto <me@sokutto.com>
2021-04-08 14:40:33 +0900

    Modified import statement and mounted() call in MapView.vue

--- frontend_vue/src/components/MapView.vue
+++ frontend_vue/src/components/MapView.vue
@@ -3,17 +3,11 @@
 </template>
 
 <script>
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
+import {main} from '@/modules/Map.js'
 
 export default {
   mounted () {
-    const map = L.map('map').setView([35.6492, 139.5493], 13)
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
-      minZoom: 5,
-      maxZoom: 18
-    }).addTo(map)
+    main()
   }
 }
 </script>
@@ -24,7 +18,5 @@ export default {
   z-index: 1;
   width: 100%;
   height: 100%;
-  border-top: 1px solid black;
-  border-bottom: 1px solid black;
 }
 </style>
