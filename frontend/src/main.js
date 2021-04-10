dbecb9083cff275d73044b60b6b6bbe2ab2ec9eb
soqutto <me@sokutto.com>
2021-04-10 10:17:46 +0900

    Installed vue-mq

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueMq from 'vue-mq'
 import App from './App'
 // import router from './router'
 
@@ -8,6 +9,15 @@ import AppStateStore from './modules/AppStateStore'
 Vue.observable(AppStateStore)
 
 Vue.config.productionTip = false
+
+Vue.use(VueMq, {
+  breakpoints: {
+    sp: 480,
+    md: 720,
+    pc: Infinity
+  },
+  defaultBreakpoint: 'sp'
+})
 
 /* eslint-disable no-new */
 new Vue({
